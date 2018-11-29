c6bf367fb3339d2f02d609bfdc1cc0eb5d53cb6b
Ofir <t0n2b@ugrad.cs.ubc.ca>
2018-11-28 20:56:28 -0800

    more commands

--- command_v1.pl
+++ command_v1.pl
@@ -18,20 +18,131 @@ valid_command(deactivate, Obj, LP, L, empty, empty):- action(Obj,power) , member
 valid_command(equip, L, empty, empty, with, Obj):- location(L), object(Obj).
 valid_command(drop, Obj, LP, L, empty, empty):- location(L), object(Obj), member(LP, [in,of]).
 valid_command(set, volume, of, Obj, to, Size):- action(Obj, volume_up), number(Size). 
+valid_command(set, channel, of, Obj, to, Size):- action(Obj, channel_up), number(Size). 
+valid_command(set, Obj, to, channel_num(Num), empty, empty):- action(Obj, channel_up), number(Num).
 valid_command(set, Obj, empty, empty, to, Size):- action(Obj, increase), number(Size).
 valid_command(set, Obj, LP, L, to, Size):- action(Obj, increase), number(Size), member(LP, [in, of]), location(L).
+valid_command(play, music, empty, empty, empty, empty).
+valid_command(play, Song, empty, empty, empty, empty):- song(Song).
+valid_command(pause, music, empty, empty, empty, empty).
 
-exe(activate,Obj,empty,empty,_, L):- location(L), object(Obj), equiped(L,Obj), retractall(state(L,Obj,power,_)), assertz(state(L,Obj,power,on)).
-exe(activate,Obj,empty,empty,Location, empty):- location(Location), object(Obj), equiped(Location,Obj), retractall(state(Location,Obj,power,_)), assertz(state(Location,Obj,power,on)).
-exe(deactivate,Obj,empty,empty,_, L):- location(L), object(Obj), equiped(L,Obj), retractall(state(L,Obj,power,_)), assertz(state(L,Obj,power,off)).
-exe(deactivate,Obj,empty,empty,Location, empty):- location(Location), object(Obj), equiped(Location,Obj), retractall(state(Location,Obj,power,_)), assertz(state(Location,Obj,power,off)).
-exe(equip,Location, with, Obj, L, empty):- location(L), object(Obj), assertz(equiped(Location, Obj)).
-exe(drop, Obj, empty, empty, _, L):- location(L), object(Obj), retractall(equiped(L,Obj)).
-exe(drop, Obj, empty, empty, Location, L):- location(Location), object(Obj), retractall(equiped(L,Obj)).
-exe(drop, Obj, empty, empty, Location, empty):- location(Location), object(Obj), retractall(equiped(Location,Obj)).
-exe(set, volume, to, Size, Location, Obj):- location(Location), equiped(Location,Obj), state(Location,Obj,power,on), retractall(state(Location,Obj,volume,_)), assertz(state(Location,Obj,volume,Size)).
-exe(set, Obj, to, Size, _, L):- location(L), object(Obj), action(Obj, increase), number(Size), equiped(L, Obj), state(L, Obj, power, on), retractall(state(L,Obj,temperature,_)), assertz(state(L,Obj,temperature,_)). 
-exe(set, Obj, to, Size, Location, empty):- location(Location), object(Obj), action(Obj, increase), number(Size), equiped(Location, Obj), state(Location, Obj, power, on), retractall(state(Location,Obj,temperature,_)), assertz(state(Location,Obj,temperature,_)). 
+exe(activate,Obj,empty,empty,_, L):- 
+    location(L), 
+    object(Obj), 
+    equiped(L,Obj),
+    retractall(state(L,Obj,power,_)),
+    assertz(state(L,Obj,power,on)).
+
+exe(activate,Obj,empty,empty,Location, empty):- 
+    location(Location), 
+    object(Obj), 
+    equiped(Location,Obj), 
+    retractall(state(Location,Obj,power,_)), 
+    assertz(state(Location,Obj,power,on)).
+
+exe(deactivate,Obj,empty,empty,_, L):- 
+    location(L), 
+    object(Obj), 
+    equiped(L,Obj), 
+    retractall(state(L,Obj,power,_)), 
+    assertz(state(L,Obj,power,off)).
+	
+exe(deactivate,Obj,empty,empty,Location, empty):- 
+    location(Location), 
+    object(Obj), 
+    equiped(Location,Obj), 
+    retractall(state(Location,Obj,power,_)), 
+    assertz(state(Location,Obj,power,off)).
+	
+exe(equip,Location, with, Obj, L, empty):- 
+    location(L), 
+    object(Obj), 
+    assertz(equiped(Location, Obj)).
+	
+exe(drop, Obj, empty, empty, _, L):- 
+    location(L), 
+    object(Obj), 
+    retractall(equiped(L,Obj)).
+	
+exe(drop, Obj, empty, empty, Location, L):- 
+    location(Location), 
+    object(Obj), 
+    retractall(equiped(L,Obj)).
+	
+exe(drop, Obj, empty, empty, Location, empty):- 
+    location(Location), 
+    object(Obj), 
+    retractall(equiped(Location,Obj)).
+	
+exe(set, volume, to, Size, Location, Obj):- 
+    location(Location), 
+    equiped(Location,Obj), 
+    state(Location,Obj,power,on), 
+    retractall(state(Location,Obj,volume,_)), 
+    assertz(state(Location,Obj,volume,Size)).
+
+exe(set, channel, to, Size, Location, Obj):- 
+    location(Location), 
+    equiped(Location,Obj), 
+    state(Location,Obj,power,on), 
+    retractall(state(Location,Obj,channel,_)), 
+    assertz(state(Location,Obj,channel,Size)).
+	
+exe(set, Obj, empty, empty, Location, channel_num(Num)):-
+    location(Location),
+    equiped(Location,Obj),
+	state(Location, Obj, power, on),
+	retractall(state(Location,Obj,channel,_)),
+	assertz(state(Location,Obj,channel,Num)).
+
+exe(set, Obj, to, Size, _, L):- 
+    location(L), 
+    object(Obj), 
+    action(Obj, increase), 
+    number(Size), 
+    equiped(L, Obj), 
+    state(L, Obj, power, on), 
+    retractall(state(L,Obj,temperature,_)), 
+    assertz(state(L,Obj,temperature,_)). 
+
+exe(set, Obj, to, Size, Location, empty):- 
+    location(Location), 
+    object(Obj), 
+    action(Obj, increase), 
+    number(Size), 
+    equiped(Location, Obj), 
+    state(Location, Obj, power, on), 
+    retractall(state(Location,Obj,temperature,_)), 
+    assertz(state(Location,Obj,temperature,_)). 
+
+exe(play, music, empty, empty, Location, empty):-
+    location(Location),
+    equiped(Location, speaker),
+    retractall(state(Location,speaker,power,_)),
+    retractall(state(Location,speaker,playing,_)),	
+    retractall(state(Location,speaker,song_played,_)),
+    assertz(state(Location, speaker, power, on)),
+    assertz(state(Location, speaker, playing, true)),
+    assertz(state(Location, speaker, song_played, default_song)).
+	
+exe(pause, music, empty, empty, Location, empty):-
+    location(Location),
+    equiped(Location, speaker),
+	state(Location,speaker,power,on),
+    retractall(state(Location,speaker,playing,_)),	
+    assertz(state(Location, speaker, playing, false)).
+
+exe(play, Song, empty, empty, empty, Location, empty):-
+    location(Location),
+    equiped(Location, speaker),
+	song(Song),
+    retractall(state(Location,speaker,power,_)),
+    retractall(state(Location,speaker,playing,_)),	
+    retractall(state(Location,speaker,song_played,_)),
+    assertz(state(Location, speaker, power, on)),
+    assertz(state(Location, speaker, playing, true)),
+    assertz(state(Location, speaker, song_played, Song)).
+
 % Prolog representation of a context-free grammar for a very restricted subset of English
 
 % This is slightly expanded code of Figures 13.7 & 13.8 in Section 13.6.1 of
@@ -95,8 +206,13 @@ noun_c([television | L], L, tv).
 noun_c([ac | L], L, ac).
 noun_c([air, conditioner | L], L, ac).
 noun_c([volume | L], L, volume).
+noun_c([speakers | L], L, speaker).
+noun_c([speaker | L], L, speaker).
+noun_c([music | L], L, music).
 noun_c([Location | L], L, Location):- location(Location).
 noun_c([Number | L], L, Number):- number(Number).
+noun_c([channel, Num | L], L, channel_num(22)):- number(Num).
+nouc_c(S, L, Song):- append(Song, L, S), song(Song).
 
 adj([practical | L],L).
 adj([new | L],L).
@@ -104,12 +220,15 @@ adj([computer, science | L],L).
 
 verb([turn, on | L],L, activate).
 verb([activate | L],L, activate).
+verb([open | L], L, activate).
 verb([turn, off | L],L, deactivate).
 verb([deactivate | L], L, deactivate).
 verb([strat | L], L, start).
 verb([equip | L], L, equip).
 verb([drop | L], L, drop).
 verb([set | L], L, set).
+verb([play | L], L, play).
+verb([pause | L], L, pause).
 
 preposition([in | L],L, in).
 preposition([on | L],L, on).
